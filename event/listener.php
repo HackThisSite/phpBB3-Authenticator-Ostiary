3ca468288ea3491a92aae30eaa46d8a22caf5343
weekend <weekend.hacker@gmail.com>
2018-05-11 16:04:21 +0200

    removed debug message

--- event/listener.php
+++ event/listener.php
@@ -67,8 +67,6 @@ class listener implements EventSubscriberInterface
         $this->user->session_create($row['user_id'], false, true, true);
         redirect($phpbb_root_path); 
       } else {
-      
-        file_put_contents('dump.log', 'FAIL: '.$this->ostiary->config['ostiary_login_url']."\r\n", FILE_APPEND | LOCK_EX);
         redirect($this->ostiary->config['ostiary_login_url'], false, true);
       }
     }
